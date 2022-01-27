5f89b5a9214564b80d5a94b711fffac88beed2e5
LAPTOP-73S7VOMB\30698 <schizast460@gmail.com>
2022-01-27 16:04:40 +0200

    upload NewsApp

---
+++ NewsApp/src/main/java/gr/unipi/newsapp/Application.java
@@ -0,0 +1,80 @@
+package gr.unipi.newsapp;
+
+import java.util.List;
+import java.util.Scanner;
+
+import exception.NewsAPIException;
+import model.NewsInfo;
+import services.NewsAPIService;
+import newsapi.NewsAPI;
+
+
+public class Application {
+
+	public static void main(String[] args) {
+		final NewsAPIService newsDBService = NewsAPI.getNewsDBService();
+
+		System.out.println("Welcome the the NewsApp");
+
+		while (true) {
+			System.out.println("Select one from the following options: ");
+			System.out.println("1. Show top headline NEWS for specified country and category.");
+			System.out.println("2. Search for NEWS.");
+			System.out.println("3. Exit.");
+			System.out.println("Your choice: ");
+
+			Scanner sc = new Scanner(System.in);
+			String input = sc.nextLine();
+
+			System.out.println("Your choice: " + input);
+
+			switch (input) {
+			case "1":
+				System.out.println("Selected 1");
+				System.out.println("Enter Country: ");
+				try {
+					final String countryInput = sc.nextLine();
+					System.out.println("Enter Category: ");
+					final String categoryInput = sc.nextLine();
+					List<NewsInfo> results;
+					results = newsDBService.searchForPopularNews(null,null,null,null,null,countryInput,categoryInput);
+					System.out.println("Results are: ");
+					System.out.println(results);
+				} catch (NewsAPIException e) {
+					System.err.println(e.getMessage());
+				}
+				
+				break;
+			case "2":
+				System.out.println("Selected 2");
+				System.out.print("Enter search parameter: ");
+				String searchParam = sc.nextLine();
+				try {
+					System.out.print("Enter search Source: ");
+					String sourceParam = sc.nextLine();
+					System.out.print("Enter search Language: ");
+					String languageParam = sc.nextLine();
+					System.out.print("Enter search Start Date: ");
+					String fromParam = sc.nextLine();
+					System.out.print("Enter search End Date: ");
+					String toParam = sc.nextLine();
+				
+					final List<NewsInfo> results = newsDBService.searchForNews(searchParam,sourceParam,languageParam,fromParam,toParam,null,null);
+					System.out.println("Results are: ");
+					System.out.println(results);
+				} catch (NewsAPIException e) {
+					System.err.println(e.getMessage());
+				}
+				break;
+			case "3":
+				System.out.println("Selected 3");
+				System.exit(0);
+				break;
+			default:
+				System.out.println("Invalid input");
+			}
+
+		}
+	}
+
+}
