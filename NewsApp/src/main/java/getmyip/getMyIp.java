ede514a182bc38b7a4b03fa52661ed6ddc43e7bd
LAPTOP-73S7VOMB\30698 <schizast460@gmail.com>
2022-01-28 12:26:07 +0200

    version2

---
+++ NewsApp/src/main/java/getmyip/getMyIp.java
@@ -0,0 +1,51 @@
+package getmyip;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class getMyIp
+{
+	public static String getCountry() throws UnknownHostException	{
+		// Returns the instance of InetAddress containing
+		// local host name and address
+		InetAddress localhost = InetAddress.getLocalHost();
+		//System.out.println("System IP Address : " +
+					//(localhost.getHostAddress()).trim());
+
+		// Find public IP address
+		String systemipaddress = "";
+		try
+		{
+			URL url_name = new URL("http://checkip.amazonaws.com");
+			//URL url_name = new URL("https://ipinfo.io/130.43.62.192/country?token=f8a6ccc2101486");
+			BufferedReader sc =
+			new BufferedReader(new InputStreamReader(url_name.openStream()));
+
+			// reads system IPAddress
+			systemipaddress = sc.readLine().trim();
+		}
+		catch (Exception e)
+		{
+			systemipaddress = "Cannot Execute Properly";
+		}
+		//System.out.println("Public IP Address: " + systemipaddress +"\n");
+		
+		String systemcountry = "";
+		try
+		{
+			URL url_name = new URL("https://ipinfo.io/"+ systemipaddress +"/country?token=f8a6ccc2101486");
+			BufferedReader sc =
+			new BufferedReader(new InputStreamReader(url_name.openStream()));
+
+			// reads system IPAddress
+			systemcountry = sc.readLine().trim();
+		}
+		catch (Exception e)
+		{
+			systemcountry = "Cannot Execute Properly";
+		}
+		//System.out.println("Public IP Address: " + systemcountry +"\n");
+		return systemcountry;
+	}
+}
--- NewsApp/src/main/java/gr/unipi/newsapp/Application.java
+++ NewsApp/src/main/java/gr/unipi/newsapp/Application.java
@@ -1,5 +1,6 @@
 package gr.unipi.newsapp;
 
+import java.net.UnknownHostException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -7,20 +8,21 @@ import exception.NewsAPIException;
 import model.NewsInfo;
 import services.NewsAPIService;
 import newsapi.NewsAPI;
-
+import getmyip.getMyIp;
 
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
 		final NewsAPIService newsDBService = NewsAPI.getNewsDBService();
-
+		
 		System.out.println("Welcome the the NewsApp");
 
 		while (true) {
 			System.out.println("Select one from the following options: ");
 			System.out.println("1. Show top headline NEWS for specified country and category.");
 			System.out.println("2. Search for NEWS.");
-			System.out.println("3. Exit.");
+			System.out.println("3. Show top headline NEWS from your Country.");
+			System.out.println("4. Exit.");
 			System.out.println("Your choice: ");
 
 			Scanner sc = new Scanner(System.in);
@@ -67,13 +69,23 @@ public class Application {
 				}
 				break;
 			case "3":
-				System.out.println("Selected 3");
+				try {					
+					final String findCountry = getMyIp.getCountry();
+					List<NewsInfo> results;
+					results = newsDBService.searchForPopularNews(null,null,null,null,null,findCountry,null);
+					System.out.println("Results are: ");
+					System.out.println(results);
+				} catch (NewsAPIException e) {
+					System.err.println(e.getMessage());
+				}
+				break;
+			case "4":
+				System.out.println("Selected 4");
 				System.exit(0);
 				break;
 			default:
 				System.out.println("Invalid input");
 			}
-
 		}
 	}
 
